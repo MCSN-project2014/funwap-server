470f5df0478b68a4974eb72c1649891377552d76
lucarin91 <lucarin91@gmail.com>
2015-01-12 14:17:14 +0100

    add timeout on the server

--- funwap/FunwapServer.py
+++ funwap/FunwapServer.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import render_template
 from flask import request
 import subprocess
+from subprocess import TimeoutExpired
 import platform
 
 app = Flask(__name__)
@@ -9,7 +10,8 @@ app = Flask(__name__)
 @app.route('/', methods=['POST', 'GET'])
 def main():
     if request.method == 'POST':
-        dataRow = request.data
+        dataRow = request.data.decode('utf-8')
+        #print (dataRow)
 
         if dataRow == '' and request.form.get('json') != None:
             dataRow = request.form['json']
@@ -23,15 +25,21 @@ def main():
         else:
             comand = ['bin\\funwaps.exe', data[0], data[1]]
 
-        p = subprocess.Popen(comand,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-        out, err = p.communicate()
-        if err == "":
-            print('result: '+out)
-            return out
-        else:
-            print('error: '+err)
-            return "server internal error"
+        try:
+            p = subprocess.Popen(comand,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+            out, err = p.communicate(timeout=30)
+            err = err.decode('utf-8')
+            out = out.decode('utf-8')
 
-    # the code below is executed if the request method
-    # was GET or the credentials were invalid
+            if err == '':
+                print('result: ' + out)
+                return out
+            else:
+                print('error: ' + err)
+                return "server internal error"
+
+        except TimeoutExpired:
+            p.kill()
+            return 'operation to long'
+
     return render_template('index.html')
